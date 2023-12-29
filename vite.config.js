f28f34157193be00b1b24c509517eaadf608f4e8
Zaid mushtaq <zaidmushtaq890@gmail.com>
2023-12-30 00:35:01 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  base: '/Zaid-Mushtaq-Portfolio/'
+  // base: '/Zaid-Mushtaq-Portfolio/'
 })
